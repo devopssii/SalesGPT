f93e0e3ba9db8b25c07f5e8c54f035afa3c1d985
devopssii <133768745+devopssii@users.noreply.github.com>
2023-07-30 18:45:08 +0500

    Update stages.py

--- salesgpt/stages.py
+++ salesgpt/stages.py
@@ -1,13 +1,13 @@
-# Example conversation stages for the Sales Agent
-# Feel free to modify, add/drop stages based on the use case.
+# Примеры стадий разговора для агента по продажам
+# Не стесняйтесь модифицировать, добавлять/удалять стадии в зависимости от конкретного случая.
 
 CONVERSATION_STAGES = {
-    "1": "Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming. Always clarify in your greeting the reason why you are calling.",
-    "2": "Qualification: Qualify the prospect by confirming if they are the right person to talk to regarding your product/service. Ensure that they have the authority to make purchasing decisions.",
-    "3": "Value proposition: Briefly explain how your product/service can benefit the prospect. Focus on the unique selling points and value proposition of your product/service that sets it apart from competitors.",
-    "4": "Needs analysis: Ask open-ended questions to uncover the prospect's needs and pain points. Listen carefully to their responses and take notes.",
-    "5": "Solution presentation: Based on the prospect's needs, present your product/service as the solution that can address their pain points.",
-    "6": "Objection handling: Address any objections that the prospect may have regarding your product/service. Be prepared to provide evidence or testimonials to support your claims.",
-    "7": "Close: Ask for the sale by proposing a next step. This could be a demo, a trial or a meeting with decision-makers. Ensure to summarize what has been discussed and reiterate the benefits.",
-    "8": "End conversation: It's time to end the call as there is nothing else to be said.",
+    "1": "Введение: Начни разговор с представления себя и своей компании. Будь вежлив и уважителен, сохраняя профессиональный тон разговора. Твое приветствие должно быть приветливым. Всегда уточняй в своем приветствии причину твоего звонка.",
+    "2": "Квалификация: Квалифицируй потенциального клиента, подтвердив, что он является правильным человеком для обсуждения твоего товара/услуги. Убедись, что у него есть полномочия для принятия решений о покупке.",
+    "3": "Предложение ценности: Кратко объясни, как твой товар/услуга может быть полезен для потенциального клиента. Сосредоточься на уникальных продажных аргументах и предложении ценности твоего товара/услуги, которые отличают его от конкурентов.",
+    "4": "Анализ потребностей: Задай открытые вопросы, чтобы выявить потребности и проблемы потенциального клиента. Внимательно слушай их ответы и делай заметки.",
+    "5": "Презентация решения: На основе потребностей потенциального клиента представь свой товар/услугу как решение, которое может решить их проблемы.",
+    "6": "Обработка возражений: Ответь на любые возражения, которые у потенциального клиента могут быть в отношении твоего товара/услуги. Будь готов предоставить доказательства или отзывы, подтверждающие твои утверждения.",
+    "7": "Завершение: Попроси о продаже, предложив следующий шаг. Это может быть демонстрация, пробный период или встреча с лицами, принимающими решения. Обязательно подведи итог тому, что было обсуждено, и повтори преимущества.",
+    "8": "Завершение разговора: Пришло время завершить звонок, так как больше нечего сказать.",
 }
